09f385615560b984eb796ed5324da6b7e2351e8f
Kristian Harrel <projzkhrrel@gmail.com>
2022-03-15 22:03:01 +0800

    Added Dashboard.

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,6 @@
 // Forms.
 export { default as LoginForm } from "./User/Form/LoginForm"
 export { default as RegistrationForm } from "./User/Form/RegistrationForm"
+
+// Profile.
+export { default as Header } from "./User/Header/Header"
