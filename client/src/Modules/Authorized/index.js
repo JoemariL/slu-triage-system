d48aaa813dc319fc699aa3fd9a217f31f855a4ce
Joemari Lopez <99390805+JoemariL@users.noreply.github.com>
2022-04-07 11:40:27 +0800

    Delete client directory

--- client/src/Modules/Authorized/index.js
+++
@@ -1,3 +0,0 @@
-export { default as LoginModule } from "./Login/LoginModule";
-export { default as RegisterModule } from "./Register/RegisterModule";
-export { default as HDFModule } from "./HDF/HDFModule";
