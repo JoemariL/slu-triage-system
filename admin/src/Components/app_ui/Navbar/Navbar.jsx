13b2159e17859ff6275d6f0037f4086901d2dcca
Joemari Lopez <lopezjoemari1@gmail.com>
2022-05-23 14:53:23 +0800

    Update Navbar.jsx

--- admin/src/Components/app_ui/Navbar/Navbar.jsx
+++ admin/src/Components/app_ui/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate, NavLink } from "react-router-dom";
 import { IoLogOutOutline } from "react-icons/io5";
 import classnames from "classnames";
 
@@ -27,89 +27,89 @@ const Navbar = () => {
     }
   };
 
-  const navigateMain = (e) => {
-    e.preventDefault();
-    toggleIsActive(1);
-    navigate("/main", { replace: true });
-  };
-
-  const navigateQR = (e) => {
-    e.preventDefault();
-    toggleIsActive(2);
-    navigate("/triageapp-qrcodes", { replace: true });
-  };
-
-  const navigateReports = (e) => {
-    e.preventDefault();
-    toggleIsActive(3);
-    navigate("/reports", { replace: true });
-  };
-
-  const navigateAppUsers = (e) => {
-    e.preventDefault();
-    toggleIsActive(4);
-    navigate("/triageapp-users", { replace: true });
-  };
-
-  const navigateAdministrators = (e) => {
-    e.preventDefault();
-    toggleIsActive(5);
-    navigate("/triageapp-administrators", { replace: true });
-  };
-
   return (
     <>
       <nav className="bg-blue-900 sticky top-0 py-5 px-3 z-40 ... text-white">
         <ul className="w-full ... inline-flex items-center gap-x-3">
-          <li
-            className={classnames(
-              "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
-              isActive === 1 && "bg-slate-100 text-black"
-            )}
-            onClick={navigateMain}
+          <NavLink
+            to="/main"
+            style={({ isActive }) => {
+              if (isActive) toggleIsActive(1);
+            }}
           >
-            DASHBOARD
-          </li>
+            <li
+              className={classnames(
+                "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
+                isActive === 1 && "bg-slate-100 text-black"
+              )}
+            >
+              DASHBOARD
+            </li>
+          </NavLink>
 
-          <li
-            className={classnames(
-              "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
-              isActive === 2 && "bg-slate-100 text-black"
-            )}
-            onClick={navigateQR}
+          <NavLink
+            to="/triageapp-qrcodes"
+            style={({ isActive }) => {
+              if (isActive) toggleIsActive(2);
+            }}
           >
-            CAMPUS & GATE
-          </li>
+            <li
+              className={classnames(
+                "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
+                isActive === 2 && "bg-slate-100 text-black"
+              )}
+            >
+              CAMPUS & GATE
+            </li>
+          </NavLink>
 
-          <li
-            className={classnames(
-              "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
-              isActive === 3 && "bg-slate-100 text-black"
-            )}
-            onClick={navigateReports}
+          <NavLink
+            to="/reports"
+            style={({ isActive }) => {
+              if (isActive) toggleIsActive(3);
+            }}
           >
-            REPORTS
-          </li>
+            <li
+              className={classnames(
+                "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
+                isActive === 3 && "bg-slate-100 text-black"
+              )}
+            >
+              REPORTS
+            </li>
+          </NavLink>
 
-          <li
-            className={classnames(
-              "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
-              isActive === 4 && "bg-slate-100 text-black"
-            )}
-            onClick={navigateAppUsers}
+          <NavLink
+            to="/triageapp-users"
+            style={({ isActive }) => {
+              if (isActive) toggleIsActive(4);
+            }}
           >
-            TRIAGE USERS
-          </li>
+            <li
+              className={classnames(
+                "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
+                isActive === 4 && "bg-slate-100 text-black"
+              )}
+            >
+              TRIAGE USERS
+            </li>
+          </NavLink>
 
-          <li
-            className={classnames(
-              "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
-              isActive === 5 && "bg-slate-100 text-black"
-            )}
-            onClick={navigateAdministrators}
+          <NavLink
+            to="/triageapp-administrators"
+            style={({ isActive }) => {
+              if (isActive) toggleIsActive(5);
+            }}
           >
-            TRIAGE TEAM
-          </li>
+            <li
+              className={classnames(
+                "py-2 px-3 rounded cursor-pointer hover:bg-slate-100 hover:text-black",
+                isActive === 5 && "bg-slate-100 text-black"
+              )}
+            >
+              TRIAGE TEAM
+            </li>
+          </NavLink>
 
           <div className="ml-auto">
             <li
