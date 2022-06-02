acd73ca62baa2aa43ef1c6a7cd874c9be6b67f89
Kristian Harrel Zuniga <projzkhrrel@gmail.com>
2022-06-03 00:01:08 +0800

    designed the about page in client..

--- client/src/Pages/Introduction.jsx
+++ client/src/Pages/Introduction.jsx
@@ -73,7 +73,7 @@ function Introduction() {
                     className="text-xl font-bold cursor-pointer"
                     onClick={homePage}
                   >
-                    Login
+                    Lo<span className="underline underline-offset-8 decoration-4 decoration-blue-900">gi</span>n
                   </li>
                   <li className="cursor-pointer" onClick={aboutPage}>
                     About Us
@@ -135,7 +135,7 @@ function Introduction() {
                     className="text-xl font-bold cursor-pointer"
                     onClick={aboutPage}
                   >
-                    About Us
+                    Abo<span className="underline underline-offset-8 decoration-4 decoration-blue-900">ut</span> Us
                   </li>
                 </div>
               </ul>
